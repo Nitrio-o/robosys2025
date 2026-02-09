bb4d790c9cd183dc234f41c773f736aa91712add
Ryusei Abe <s21c1007qn@s.chibakoudai.jp>
2026-02-09 21:22:23 +0900

    tsdiff用のテストを追加

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: 2025 Ryusei Abe
+# SPDX-FileCopyrightText: 2026 Ryusei Abe
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
@@ -10,25 +10,25 @@ ng () {
 res=0
 
 ### NORMAL INPUT ###
-seq 5 | ./mean > .out_normal
+printf "0 a\n1 b\n3 c\n" | ./tsdiff > .out_normal
 [ "$?" = 0 ] || ng "$LINENO"
 out="$(cat .out_normal)"
-if [ "${out}" = 3 ] || [ "${out}" = 3.0 ]; then :; else ng "$LINENO"; fi
+expected=$'0\t0 a\n1\t1 b\n2\t3 c'
+[ "${out}" = "${expected}" ] || ng "$LINENO"
 
-### STRANGE INPUT 1: 非数値 ###
-echo あ | ./mean > .out_nonnum 2>/dev/null
+### STRANGE INPUT 1: 非数値タイムスタンプ ###
+echo "a b" | ./tsdiff > .out_bad 2>/dev/null
 [ "$?" = 1 ] || ng "$LINENO"
-out="$(cat .out_nonnum)"
+out="$(cat .out_bad)"
 [ -z "${out}" ] || ng "$LINENO"
 
-### STRANGE INPUT 2: 無入力 ###
-echo | ./mean > .out_empty 2>/dev/null
+### STRANGE INPUT 2: 無入力（空） ###
+echo | ./tsdiff > .out_empty 2>/dev/null
 [ "$?" = 1 ] || ng "$LINENO"
 out="$(cat .out_empty)"
 [ -z "${out}" ] || ng "$LINENO"
 
-rm -f .out_normal .out_nonnum .out_empty
+rm -f .out_normal .out_bad .out_empty
 
 [ "$res" = 0 ] && echo OK
 exit "$res"
-
